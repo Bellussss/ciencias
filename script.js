f4dd7c66d4cb6ebd1d1eeb317bdac8b3c29a2f64
Bellussss <otavio.bellucci.senai404@gmail.com>
2025-06-18 14:33:16 -0300

    semi-pronto
    falta apenas trocar as cartas

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('.card').forEach(card => {
         card.addEventListener('click', () => {
-            document.querySelectorAll(".card").forEach((c)=>{
+            document.querySelectorAll(".card").forEach((c) => {
                 if (c !== card) {
                     c.classList.remove("is-flipped");
                 }
-            });  card.classList.toggle('is-flipped');
+            }); card.classList.toggle('is-flipped');
         });
     });
 });
-
-
-function mostrarImagem(caminho) {
-    const img = document.getElementById('imagemSelecionada');
-    img.src = caminho;
-    img.style.display = 'block';
-}
-
-.botaoDev1.addEventListener('onclick')
